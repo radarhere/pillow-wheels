42958d4e3c51305783fd5ca2ac16f5a53d30193c
Andrew Murray <radarhere@users.noreply.github.com>
2021-01-31 15:18:25 +1100

    1195

--- config.sh
+++ config.sh
@@ -102,7 +102,9 @@ function run_tests {
         brew install openblas
         echo -e "[openblas]\nlibraries = openblas\nlibrary_dirs = /usr/local/opt/openblas/lib" >> ~/.numpy-site.cfg
     fi
-    if [[ "$MB_PYTHON_VERSION" != pypy3.7-* ]] || [[ "$PLAT" != "i686" ]]; then
+    if [[ "$MB_PYTHON_VERSION" == pypy3.7-* ]] && [[ "$PLAT" == "i686" ]]; then
+	    python3 -m pip install numpy==1.19.5
+    else
 	    python3 -m pip install numpy
 	fi
 
