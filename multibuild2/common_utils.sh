01d8e9f98c5b8147eecb99c305d29a3af3b49863
Andrew Murray <radarhere@users.noreply.github.com>
2021-02-15 21:12:17 +1100

    find

--- multibuild2/common_utils.sh
+++ multibuild2/common_utils.sh
@@ -364,6 +364,7 @@ function install_wheel {
     #     MANYLINUX_URL (optional, default "") (via pip_opts function)
     local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
     check_pip
+    find / -iname pip
     if [ -n "$TEST_DEPENDS" ]; then
         while read TEST_DEPENDENCY; do
             $PIP_CMD install $(pip_opts) $@ $TEST_DEPENDENCY
@@ -494,7 +495,7 @@ function install_pypy {
     if [ "$major" == "3" ] && [ ! -x "$py_build/bin/pip" ]; then
         ln $py_build/bin/pip3 $py_build/bin/pip
     fi
-    PIP_CMD=$py_build/bin/pip
+    PIP_CMD=pip
 }
 
 function fill_pypy_ver {
