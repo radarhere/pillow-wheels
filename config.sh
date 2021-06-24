92c3d2f55fad6156f128ea34e45ececb9e80dbed
Andrew Murray <radarhere@users.noreply.github.com>
2021-06-24 20:44:08 +1000

    Upgrade pip before installing numpy

--- config.sh
+++ config.sh
@@ -74,45 +74,6 @@ function pre_build {
     CFLAGS="$CFLAGS -g -O2"
     build_jpeg
     CFLAGS=$ORIGINAL_CFLAGS
-
-    if [[ -n "$IS_MACOS" && $MACOSX_DEPLOYMENT_TARGET == "11.0" ]]; then
-        TIFF_VERSION=4.2.0
-    fi
-    build_tiff
-    if [ -n "$IS_MACOS" ]; then
-        # Remove existing libpng
-        rm /usr/local/lib/libpng*
-    fi
-    build_libpng
-    build_lcms2
-    build_openjpeg
-
-    CFLAGS="$CFLAGS -O3 -DNDEBUG"
-    build_libwebp
-    CFLAGS=$ORIGINAL_CFLAGS
-
-    if [ -n "$IS_MACOS" ]; then
-        # Custom freetype build
-        build_simple freetype $FREETYPE_VERSION https://download.savannah.gnu.org/releases/freetype tar.gz --with-harfbuzz=no --with-brotli=no
-    else
-        build_freetype
-    fi
-
-    if [ -z "$IS_MACOS" ]; then
-        export FREETYPE_LIBS=-lfreetype
-        export FREETYPE_CFLAGS=-I/usr/local/include/freetype2/
-    fi
-    build_simple harfbuzz $HARFBUZZ_VERSION https://github.com/harfbuzz/harfbuzz/releases/download/$HARFBUZZ_VERSION tar.xz --with-freetype=yes --with-glib=no
-    if [ -z "$IS_MACOS" ]; then
-        export FREETYPE_LIBS=''
-        export FREETYPE_CFLAGS=''
-    fi
-
-    # Append licenses
-    for filename in dependency_licenses/*; do
-      echo -e "\n\n----\n\n$(basename $filename | cut -f 1 -d '.')\n" | cat >> Pillow/LICENSE
-      cat $filename >> Pillow/LICENSE
-    done
 }
 
 function pip_wheel_cmd {
@@ -121,8 +82,6 @@ function pip_wheel_cmd {
         CFLAGS="$CFLAGS --std=c99"  # for Raqm
     fi
     pip wheel $(pip_opts) \
-        --global-option build_ext --global-option --enable-raqm \
-        --global-option --vendor-raqm --global-option --vendor-fribidi \
         -w $abs_wheelhouse --no-deps .
 }
 
@@ -147,28 +106,12 @@ function run_tests {
         brew install openblas
         echo -e "[openblas]\nlibraries = openblas\nlibrary_dirs = /usr/local/opt/openblas/lib" >> ~/.numpy-site.cfg
     fi
-	python3 -m pip install numpy
+    python3 -m pip install --upgrade pip
+    python3 -m pip install numpy
 
     mv ../pillow-depends-master/test_images/* ../Pillow/Tests/images
 
     # Runs tests on installed distribution from an empty directory
     (cd ../Pillow && run_tests_in_repo)
-    # Test against expected codecs, modules and features
-    local ret=0
-    local codecs=$(python3 -c 'from PIL.features import *; print(" ".join(sorted(get_supported_codecs())))')
-    if [ "$codecs" != "$EXP_CODECS" ]; then
-        echo "Codecs should be: '$EXP_CODECS'; but are '$codecs'"
-        ret=1
-    fi
-    local modules=$(python3 -c 'from PIL.features import *; print(" ".join(sorted(get_supported_modules())))')
-    if [ "$modules" != "$EXP_MODULES" ]; then
-        echo "Modules should be: '$EXP_MODULES'; but are '$modules'"
-        ret=1
-    fi
-    local features=$(python3 -c 'from PIL.features import *; print(" ".join(sorted(get_supported_features())))')
-    if [ "$features" != "$EXP_FEATURES" ]; then
-        echo "Features should be: '$EXP_FEATURES'; but are '$features'"
-        ret=1
-    fi
     return $ret
 }
